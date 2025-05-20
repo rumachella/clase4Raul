3217ae545cd5df9b7272ce0b1a42cf743776a15f
Fabrizzio Rumachella <contacto.rumachella@gmail.com>
2025-05-20 01:30:04 -0300

    Creacion del endpoint de historial con proteccion de roles

--- historial-microservice.js
+++ historial-microservice.js
@@ -3,14 +3,8 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.HISTORIAL_PORT;
 const connection = require('./db');
-
-
-
-
-
 app.use(express.json());
 
-let historial = [];
 
 //*                                         POST /guardar → Recibe y guarda una operación en el historial
 app.post('/guardar', (req, res) => {
@@ -24,6 +18,7 @@ app.post('/guardar', (req, res) => {
     INSERT INTO operaciones (usuario, operacion, resultado, fecha)
     VALUES (?, ?, ?, ?)
   `;
+    console.log('BODY RECIBIDO:', req.body);
 
   connection.query(query, [user, operation, result, timestamp], (err, resultDb) => {
     if (err) {
@@ -31,22 +26,33 @@ app.post('/guardar', (req, res) => {
       return res.status(500).json({ error: 'Error al guardar en la base de datos' });
     }
 
-    const registro = { user, operation, result, timestamp };
-    historial.push(registro);
+    // const registro = { user, operation, result, timestamp };
 
     res.json({ message: 'Operación guardada correctamente' });
   });
 });
 
-//*                                 GET /historial → Devuelve el contenido del historial (en memoria)
-app.get('/historial', (req, res) => {
-  
+//*                                 GET /historial → Devuelve el contenido del historial en la bd
+//?                                 Solo el admin puede consultar el historial
 
+const { verifyToken, requireRole } = require('./auth.js');
+app.get('/historial', verifyToken, requireRole('admin'), (req, res) => {
+  const query = 'SELECT * FROM operaciones ORDER BY fecha DESC'; 
 
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al obtener historial:', err);
+      return res.status(500).json({ error: 'Error interno del servidor' });
+    }
 
-  //convierte el historial en formato JSON
-  res.json(historial);
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'No hay historial disponible' });
+    }
+
+    res.json(results);
+  });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Microservicio de historial corriendo en http://localhost:${PORT}`);
